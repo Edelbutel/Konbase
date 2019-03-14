518be0142daeb72ddc4387c607fca92a5f87cec0
Matheus Rodrigues Del Re <matheusrd97@gmail.com>
2019-03-14 20:35:54 -0300

    Commit 2
    Commit 2

--- KonBase/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ KonBase/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -36,15 +36,15 @@ namespace KonBase.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
-            [EmailAddress]
+            [Required(ErrorMessage = "Email é um campo obrigatório")]
+            [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$", ErrorMessage = "Email está em um formato inválido.")]
             public string Email { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Senha é um campo obrigatório", AllowEmptyStrings = false)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembrar")]
             public bool RememberMe { get; set; }
         }
 
