e0eb5cbf4a62599cafe4cf2b38ae23fa2ed701d5
Matheus Rodrigues Del Re <38119891+matheusdelre@users.noreply.github.com>
2019-03-14 18:08:33 -0300

    1º Commit
    1º Commit

---
+++ KonBase/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using KonBase.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(KonBase.Areas.Identity.IdentityHostingStartup))]
+namespace KonBase.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
